6f22ec54d14ccecb84620dd24087819f5c013296
“Dhivyasella14” <sellamani.dhivya@quarksek.com>
2026-08-06 12:21:22 +0530

    Fix

--- tests/verifyErrormsg.spec.ts
+++ tests/verifyErrormsg.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }: { page: any }) => {
   await page.goto('https://www.saucedemo.com/');
-  const title = page.locator('.login_logo');
   await expect(page).toHaveTitle('Swag Labs');
   await expect(page).toHaveURL('https://www.saucedemo.com/');
   await page
