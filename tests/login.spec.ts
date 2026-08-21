a61a6c0064acef0810011bac351218522ef716c1
Dhivyasella14 <sellamani.dhivya@quarksek.com>
2026-08-21 08:57:44 +0530

    Commit for fixture file (#10)
    * Commit for fixture file
    
    * Commented old code
    
    * Uncommented lines

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,8 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '../fixtures.ts';
 import { LoginPage } from '../pages/LoginPage.ts';
 
-test ("Standard user can login", async ({ page }) => {
+test.describe('Authentication', () => {
+    //let loginPage: LoginPage;
+
+    // Before hook file
+    // test.beforeEach(async ({ page }) => {
+    //     loginPage = new LoginPage(page);
+    //     await loginPage.open();
+    // });
+
+    test("Standard user can login", async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.open();
     await loginPage.login('standard_user', 'secret_sauce');
+    await expect(page).toHaveURL(/inventory.html/);
+});
 });
