d71975a8f0b6fc05f5712f6cd72d226ebf961a12
“Dhivyasella14” <sellamani.dhivya@quarksek.com>
2026-08-25 13:19:19 +0530

    problem user

--- tests/addRemoveCart.spec.ts
+++ tests/addRemoveCart.spec.ts
@@ -42,3 +42,15 @@ test('Add and remove product - using fixture', async ({ cartPage }) => {
   await cartPage.removeFromCart('Sauce Labs Bolt T-Shirt');
   await expect(cartPage.itemNames()).toContainText(['Sauce Labs Fleece Jacket']);
 });
+
+test('problem user sees the same image for all products', async ({ page }) => {
+  await page.goto('/inventory.html');
+
+  const imageSources = await page
+    .locator('.inventory_item_img img')
+    .evaluateAll((images) =>
+      images.map((image) => image.getAttribute('src'))
+    );
+  expect(imageSources).toHaveLength(6);
+  expect(new Set(imageSources).size).toBe(6);
+});
